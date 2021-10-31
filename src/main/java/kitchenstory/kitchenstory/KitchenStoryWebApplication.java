a57a6b08c17da6c39f6e243653c5e7a5660aa916
sudip34 <80578123+sudip34@users.noreply.github.com>
2021-10-31 02:14:09 +0200

    Update KitchenStoryWebApplication.java

--- src/main/java/kitchenstory/kitchenstory/KitchenStoryWebApplication.java
+++ src/main/java/kitchenstory/kitchenstory/KitchenStoryWebApplication.java
@@ -2,16 +2,21 @@ package kitchenstory.kitchenstory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 @SpringBootApplication
-public class KitchenStoryWebApplication {
+public class KitchenStoryWebApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(KitchenStoryWebApplication.class, args);
 	}
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JarToWarApp.class);
+	}
 
 }
